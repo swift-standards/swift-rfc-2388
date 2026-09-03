bcd60d72ff94eb4a481e00165e611cf4dd9a729a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 15:03:51 +0200

    Converge dependency locations to their canonical organizations

--- Package.swift
+++ Package.swift
@@ -35,17 +35,12 @@ let package = Package(
         .testTarget(
             name: "RFC 2388 Tests",
             dependencies: [
-                "RFC 2388"
+                .target(name: "RFC 2388")
             ]
         ),
     ],
     swiftLanguageModes: [.v6]
 )
-
-extension String {
-    var tests: Self { self + " Tests" }
-    var foundation: Self { self + " Foundation" }
-}
 
 for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
     let ecosystem: [SwiftSetting] = [
